5016987ae644cd79a689ba4e26b0a590bfe0202c
dionnamela <dionnamlea@gmail.com>
2025-01-16 23:51:42 +0800

    feat: create kategori produk

--- app/Livewire/Dashboard/ProductCategories.php
+++ app/Livewire/Dashboard/ProductCategories.php
@@ -3,9 +3,44 @@
 namespace App\Livewire\Dashboard;
 
 use Livewire\Component;
+use App\Models\ProductCategories as ModelProductCategories;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ProductCategories extends Component
 {
+    public $productCategories;
+    public $name;
+    public $search = '';
+    public $loading = true;  // Properti loading
+
+    public function mount()
+    {
+
+
+        // Mengambil data kategori produk
+        $this->productCategories = ModelProductCategories::all();
+
+        // Setelah data dimuat, set loading ke false
+        $this->loading = false;
+    }
+
+    public function store()
+    {
+        // Menyimpan kategori produk
+        ModelProductCategories::create([
+            'name' => $this->name
+        ]);
+
+        // Reset input setelah penyimpanan
+        $this->reset('name');
+
+        // Merefresh data kategori produk
+        $this->productCategories = ModelProductCategories::all();
+
+        // Dispatch custom event untuk memicu aksi JavaScript
+        $this->dispatch('addedSuccess');
+    }
+
     public function render()
     {
         return view('livewire.dashboard.productCategories');
--- resources/views/livewire/dashboard/productCategories.blade.php
+++ resources/views/livewire/dashboard/productCategories.blade.php
@@ -1,11 +1,158 @@
 <div>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Holla! ini halaman Product categories
+                {{-- Button Add & Search --}}
+
+                <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                    <div
+                        class="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+                        <div class="w-full md:w-1/2">
+                            <button type="button" id="defaultModalButton" data-modal-target="defaultModal"
+                                data-modal-toggle="defaultModal"
+                                class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Tambah
+                                Kategori Produk</button>
+
+                            {{-- Pencarian --}}
+                            <form class="flex items-center">
+                                <label for="simple-search" class="sr-only">Search</label>
+                                <div class="relative w-full">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor"
+                                            viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="simple-search"
+                                        class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500"
+                                        placeholder="Search" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-500 text-white">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">Nama Kategori</th>
+                                <th scope="col" class="px-6 py-3">Dibuat Pada</th>
+                                <th scope="col" class="px-6 py-3">Diubah Pada</th>
+                                <th scope="col" class="px-6 py-3">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($productCategories as $category)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $category->name }}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{ $category->created_at->format('d M Y') }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ $category->updated_at->format('d M Y') }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="#"
+                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        <a href="#"
+                                            onclick="confirm('Apakah Anda yakin ingin menghapus kategori ini?') || event.stopImmediatePropagation()"
+                                            wire:click="delete({{ $category->id }})"
+                                            class="ml-3 font-medium text-red-600 dark:text-red-500 hover:underline">Hapus</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
     </div>
+
+    {{-- SweetAlert --}}
+    @if (session()->has('message'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Kategori Produk Ditambahkan!',
+                text: '{{ session('message') }}',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        </script>
+    @endif
+
+    {{-- Modal Create --}}
+    <div id="defaultModal" tabindex="-1" aria-hidden="true"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+            <!-- Modal content -->
+            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                <!-- Modal header -->
+                <div
+                    class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                        Tambah Kategori Produk
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-toggle="defaultModal">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <form wire:submit.prevent="store">
+                    <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                        <div class="sm:col-span-2">
+                            <label for="description"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
+                                Kategori</label>
+                            <input type="text" name="name" id="name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Masukan nama kategori" required wire:model="name">
+                        </div>
+                    </div>
+                    <button type="submit"
+                        class="text-white inline-flex items-center px-4 py-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Simpan
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script>
+        Livewire.on('addedSuccess', () => {
+            // Menutup modal dengan id 'defaultModal'
+            const modal = document.getElementById('defaultModal');
+            const modalCloseButton = modal.querySelector('[data-modal-toggle="defaultModal"]');
+            modalCloseButton.click();
+
+            // Menampilkan SweetAlert setelah modal ditutup
+            Swal.fire({
+                icon: 'success',
+                title: 'Kategori Produk Ditambahkan!',
+                text: 'Kategori produk baru telah berhasil ditambahkan.',
+                showConfirmButton: true,
+                confirmButtonText: 'Tutup',
+                timer: 1500,
+                customClass: {
+                    confirmButton: 'bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-500'
+                }
+            });
+        });
+    </script>
 </div>
